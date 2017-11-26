4706b1ee7f1171a676ddbc7efce6e18f0712c9bb
BeYkeRYkt <beykerykt@gmail.com>
2017-11-26 22:38:11 +0900

    msm8998-common: remove ifaa_fingerprint
    Change-Id: Ia1491e6a502c22053fb8d2a2b2ba1d558346e569

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -2,10 +2,6 @@
 PRODUCT_PACKAGES += \
     fingerprintd
 
-# fingerprint pay
-PRODUCT_BOOT_JARS += \
-    ifaa_fingerprint
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
