16623d66917d0090e1dc99f24983c9c18b01efb4
BeYkeRYkt <beykerykt@gmail.com>
2017-12-19 22:48:36 +0900

    msm8998-common: move gps configs to vendor
    Change-Id: I76e73c0cdcf304caea83ac719d5ae83f740ea969

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,12 +1,12 @@
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/apdr.conf:system/etc/apdr.conf \
-    $(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/configs/gps/apdr.conf:system/vendor/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/flp.conf:system/vendor/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/vendor/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:system/vendor/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:system/vendor/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:system/vendor/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
 # GPS
 PRODUCT_PACKAGES += \
