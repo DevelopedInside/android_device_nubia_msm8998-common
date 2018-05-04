66625ae0a2a60aa905598c53d9c39b8b0ff2ea34
BeYkeRYkt <beykerykt@gmail.com>
2018-05-04 19:21:21 +0900

    msm8998-common: update data-ipa-cfg-mgr to 'LA.UM.5.7.r1-12100-8x98.0'
    Change-Id: I69155e066f8907066f640393e33f47e5d73e17d5

--- data-ipa-cfg-mgr/ipacm/src/IPACM_Log.cpp
+++ data-ipa-cfg-mgr/ipacm/src/IPACM_Log.cpp
@@ -1,5 +1,5 @@
 /* 
-Copyright (c) 2013, The Linux Foundation. All rights reserved.
+Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
@@ -91,7 +91,7 @@ void ipacm_log_send( void * user_data)
 		printf("create ipacm_log socket successfully\n");
 	}
 	ipacmlog_socket.sun_family = AF_UNIX;
-	strcpy(ipacmlog_socket.sun_path, IPACMLOG_FILE);
+	strlcpy(ipacmlog_socket.sun_path, IPACMLOG_FILE, sizeof(ipacmlog_socket.sun_path));
 	len = strlen(ipacmlog_socket.sun_path) + sizeof(ipacmlog_socket.sun_family);
 
 	memcpy(ipacm_log_buffer.user_data, user_data, MAX_BUF_LEN);
