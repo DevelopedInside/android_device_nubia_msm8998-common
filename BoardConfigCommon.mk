fa24a2fc229967bd9237ce067832603803a3e505
BeYkeRYkt <beykerykt@gmail.com>
2018-05-11 18:03:59 +0900

    msm8998-common: use autodetect SDClang
    * use Snapdragon LLVM, if available:
    https://github.com/LineageOS/android_vendor_cm/commit/26e669badc0154baf8fd4b7e2da84fea1d91249a
    
    Change-Id: Iaafb2784680ddf7b344409372f1751937e72b702

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,16 +29,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
 BOARD_VENDOR := nubia
 
 # Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := false
-
-# SDClang 3.8.8
-ifeq ($(TARGET_USE_SDCLANG),true)
-ifneq ($(HOST_OS),darwin)
-SDCLANG := true
-SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8.8/bin
-SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
-endif
-endif
+TARGET_USE_SDCLANG := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8998
