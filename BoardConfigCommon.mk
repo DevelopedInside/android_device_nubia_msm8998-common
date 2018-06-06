26a5a40fb82551ba383d1b4c3d62871c240467e2
BeYkeRYkt <beykerykt@gmail.com>
2018-06-06 12:55:24 +0900

    msm8998-common: revert back android_filesystem_config.h header path
    * It defaults to device/$vendor/$device/android_filesystem_config.h. Override him.
    
    Change-Id: If2bf15effa38923d81093789f96b945818ba3496

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,6 +197,9 @@ ifeq ($(HOST_OS),linux)
 endif
 WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
+# Filesystem
+TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(PLATFORM_PATH)/android_filesystem_config.h
+
 # GPS
 TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
