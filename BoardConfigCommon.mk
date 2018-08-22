eee2854e8ec6ec11c74e6cc4e6c88f68b7f14f97
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-22 13:15:37 +0200

    msm8998-common: specify additional root folders
    Change-Id: Id92cdf1ef505960f097d447f23e921030a1b447d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -255,6 +255,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 PROTOBUF_SUPPORTED := true
 TARGET_RIL_VARIANT := caf
 
+# Root
+BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
