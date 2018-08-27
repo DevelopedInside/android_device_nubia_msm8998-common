7ed72537a2483b3dd3ceabae5ff1cbd873089cf7
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-27 18:11:22 +0200

    msm8998: switch to hidl ANT+ hal
    Change-Id: I6ef945fc64f3d8f4c31d89bcdf684333aae99595

--- msm8998.mk
+++ msm8998.mk
@@ -143,8 +143,7 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
@@ -164,10 +163,6 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
