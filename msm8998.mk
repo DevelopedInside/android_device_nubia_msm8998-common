4374f61f67f2b89f257bd335cc17906d4ba80ad6
BeYkeRYkt <beykerykt@gmail.com>
2018-09-06 01:01:56 +0900

    Revert "msm8998-common: move audio_effects to specific device"
    This reverts commit 06ec15a1f7f1c8c9a9f05e3302c5473919ef6b3b.

--- msm8998.mk
+++ msm8998.mk
@@ -103,6 +103,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
