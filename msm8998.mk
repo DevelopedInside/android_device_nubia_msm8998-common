0550303317fea5339b2ae5d0852e65093938f730
BeYkeRYkt <beykerykt@gmail.com>
2018-09-17 15:08:00 +0900

    msm8998-common: build android.hidl.manager@1.0-java
    Change-Id: I58efbb78bab94b39cf50d112d71aa908032cca54

--- msm8998.mk
+++ msm8998.mk
@@ -231,7 +231,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # IMS
 PRODUCT_PACKAGES += \
