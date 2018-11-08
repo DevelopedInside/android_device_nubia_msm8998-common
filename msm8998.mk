f669edd41df6cc0ec226dee2f2e522e201a111c4
BeYkeRYkt <beykerykt@gmail.com>
2018-11-08 20:09:24 +0900

    msm8998-common: remove weaver
    1220  1220 I system_server: Looking for service android.hardware.weaver@1.0::IWeaver/default
    1220  1220 I SyntheticPasswordManager: Device does not support weaver
    
    Change-Id: I8a39afa7039467b9e6d2d8080b0274c773237f6f

--- msm8998.mk
+++ msm8998.mk
@@ -423,10 +423,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-service \
     vr.msm8998
 
-# Weaver
-PRODUCT_PACKAGES += \
-    android.hardware.weaver@1.0
-
 # Wifi
 PRODUCT_PACKAGES += \
     hostapd \
