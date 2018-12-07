facb8f3f5344e3665d14f182673f51a77110f340
BeYkeRYkt <beykerykt@gmail.com>
2018-12-08 05:40:38 +0900

    msm8998-common: fix typo
    Change-Id: I8ede3d009eccdf7f12c78a27aa71b739cc648c2e

--- msm8998.mk
+++ msm8998.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
-    android.hardware.gnss@1.0-service-qti \
+    android.hardware.gnss@1.0-service-qti
 
 PRODUCT_PACKAGES += \
     gps.conf \
