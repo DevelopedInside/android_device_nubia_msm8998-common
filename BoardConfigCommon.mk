a1b382cb411b9c3562ca0053a1bd80b62bc1f481
Kilian von Pflugk <github@jumoog.io>
2018-12-10 12:31:27 +0100

    msm8998-common: remove TARGET_USES_64_BIT_BINDER
    the TARGET_USES_64_BIT_BINDER option is deprecated and can be removed
    
    Change-Id: I6b828da1fbcc8c054c5b35ff78c870a689f7ed9f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,8 +52,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
-TARGET_USES_64_BIT_BINDER := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 
