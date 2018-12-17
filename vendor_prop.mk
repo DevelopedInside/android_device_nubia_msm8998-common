a3627a75ef65c7c4dd286a9af15d50807b564633
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    Revert "msm8998-common: Add system properties for surfaceflinger"
    This reverts commit 2a9cde9a8e895b5f449ca875708d7c0af953bbff.
    
    Change-Id: I4b9bb06a047ccb14a89e693bca7e2d4629971456

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -201,9 +201,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.protected_contents=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \
