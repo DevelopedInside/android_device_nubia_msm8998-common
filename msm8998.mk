050763070286019082a0f45f6772e2a6f180b1b4
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    msm8998-common: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- msm8998.mk
+++ msm8998.mk
@@ -237,6 +237,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
