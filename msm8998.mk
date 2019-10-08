9378371250d456b8734c622d9b1212987dc74492
Michael Bestas <mkbestas@lineageos.org>
2019-10-08 21:47:36 +0300

    msm8998-common: Remove android.hidl.base@1.0 shims
    Change-Id: I00f185fd4d1d8798893cf82abf9d64fcb68c3301

--- msm8998.mk
+++ msm8998.mk
@@ -234,13 +234,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
-
 # HW crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
