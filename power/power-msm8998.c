ad69625cd751589c84b319b0586371b1d32a5ce4
BeYkeRYkt <beykerykt@gmail.com>
2019-10-25 22:14:41 +0900

    msm8998-common: power: back to lineageos revision
    Change-Id: I767a99e30bc564d666c786b331cd9ed41e7cc522

--- power/power-msm8998.c
+++ power/power-msm8998.c
@@ -54,7 +54,7 @@
 #define CHECK_HANDLE(x) ((x)>0)
 #define NUM_PERF_MODES  3
 
-const int kMaxInteractiveDuration = 5160; /* ms */
+const int kMaxInteractiveDuration = 5000; /* ms */
 const int kMinInteractiveDuration = 500; /* ms */
 
 static int current_power_profile = PROFILE_BALANCED;
@@ -325,12 +325,9 @@ static int process_interaction_hint(void *data)
 
     if (data) {
         int input_duration = *((int*)data);
-        if (input_duration > 0) {
-            perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, -1, SCROLL_PREFILING);
-        }
         if (input_duration > duration) {
             duration = (input_duration > kMaxInteractiveDuration) ?
-                    kMaxInteractiveDuration : (input_duration + 160);
+                    kMaxInteractiveDuration : input_duration;
         }
     }
 
@@ -344,11 +341,7 @@ static int process_interaction_hint(void *data)
     s_previous_boost_timespec = cur_boost_timespec;
     s_previous_duration = duration;
 
-    if (duration > kMinInteractiveDuration) {
-        perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
-    } else {
-        perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_HORIZONTAL);
-    }
+    perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
 
     return HINT_HANDLED;
 }
