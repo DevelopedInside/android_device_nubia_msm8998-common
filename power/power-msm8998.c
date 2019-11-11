bd5b4e1022ec5783cf5f4ad116fb0cbe18610dd9
BeYkeRYkt <beykerykt@gmail.com>
2019-11-11 21:34:06 +0900

    msm8998-common: power: perf tune
    * Use a minimum time of 160 ms for touchboost, and add a value to it if fling is detected.
    * In the CAF branch, SCROLL_PREFILING is used during interface redrawing, when
      user interaction occurs or a possible flinging. Use this hint during a
      short interaction.
    * Use SCROLL_VERTICAL if we have scrolling duration information according to CAF.
    * For some SoC or devices, the prompt variables may be different.
    
    Change-Id: Id8e877963847cc09ac79c802f34a57ee3d9d3f14

--- power/power-msm8998.c
+++ power/power-msm8998.c
@@ -55,7 +55,7 @@
 #define NUM_PERF_MODES  3
 
 const int kMaxInteractiveDuration = 5000; /* ms */
-const int kMinInteractiveDuration = 500; /* ms */
+const int kMinInteractiveDuration = 160; /* ms */
 
 static int current_power_profile = PROFILE_BALANCED;
 
@@ -325,8 +325,8 @@ static int process_interaction_hint(void *data)
 
     if (data) {
         int input_duration = *((int*)data);
-        if (input_duration > duration) {
-            duration = (input_duration > kMaxInteractiveDuration) ?
+        if (input_duration > 0) {
+            duration += (input_duration > kMaxInteractiveDuration) ?
                     kMaxInteractiveDuration : input_duration;
         }
     }
@@ -341,7 +341,11 @@ static int process_interaction_hint(void *data)
     s_previous_boost_timespec = cur_boost_timespec;
     s_previous_duration = duration;
 
-    perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
+    if (duration > kMinInteractiveDuration) {
+        perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
+    } else {
+        perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_PREFILING);
+    }
 
     return HINT_HANDLED;
 }
