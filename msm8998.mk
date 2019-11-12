af28194e52290bb56b7b366b507934431c7ec5b9
Quallenauge <Hamsi2k@freenet.de>
2019-11-12 22:49:17 +0100

    Revert "msm8998-common: Exclude Snap overlays from RRO"
    This reverts commit abe910cebd155269efc4c58942774679c668219b.
    
    Change-Id: Ic7738e31cc93e7ca01d7ee72c2dab024a07175d1

--- msm8998.mk
+++ msm8998.mk
@@ -27,8 +27,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
