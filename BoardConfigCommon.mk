3528b91f9dfe8aeacb2c1baf5e4c175f808e493b
Michael Bestas <mkbestas@lineageos.org>
2019-12-16 17:49:45 +0200

    msm8998-common: sepolicy: Label dpmd
    * P blob compatibility
    
    Change-Id: I884f638e0aeb2f7bfb48b2b1d186b7932fe9a705

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -263,6 +263,7 @@ VENDOR_SECURITY_PATCH := 2019-02-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
