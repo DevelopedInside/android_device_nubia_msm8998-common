13ea504478205ec0ee3f67059766d5ba6f2405ba
BeYkeRYkt <beykerykt@gmail.com>
2019-12-27 17:47:33 +0900

    msm8998-common: fix frp partition path
    Change-Id: If1b1ef1bfb18d1242efcb8b44aa5cc81bc348b1c

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -93,7 +93,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/config
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
