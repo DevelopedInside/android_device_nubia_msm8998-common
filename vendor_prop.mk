a231a32d71c540b0e41a7c301a8923e9846a771d
BeYkeRYkt <beykerykt@gmail.com>
2020-02-07 14:43:04 +0900

    msm8998-common: alphabetize properties
    Change-Id: I6cf97b7a44e344d666e7332b8e123c049e5665a3

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -17,40 +17,40 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=192 \
-    ro.vendor.audio.sdk.fluencetype=fluence \
-    persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.fluence.speaker=false \
-    vendor.audio.tunnel.encode=false \
-    persist.vendor.audio.ras.enabled=false \
-    vendor.audio.offload.buffer.size.kb=32 \
+    audio.deep_buffer.media=true \
     audio.offload.min.duration.secs=30 \
     audio.offload.video=true \
-    vendor.audio.offload.track.enable=true \
-    audio.deep_buffer.media=true \
-    vendor.voice.path.for.pcm.voip=true \
-    vendor.audio.offload.multiaac.enable=true \
+    persist.vendor.audio.fluence.speaker=false \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.hw.binder.size_kbyte=1024 \
+    persist.vendor.audio.ras.enabled=false \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    ro.af.client_heap_size_kbyte=7168 \
+    ro.config.vc_call_vol_steps=11 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.adm.buffering.ms=3 \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.noise.reduction.for.voice.call=false \
+    vendor.audio.noisy.broadcast.delay=600 \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
     vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
-    ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.offload.gapless.enabled=true \
-    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.offload.pstimeout.secs=3 \
+    vendor.audio.offload.track.enable=true \
     vendor.audio.parser.ip.buffer.size=262144 \
-    vendor.audio.flac.sw.decoder.24bit=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
     vendor.audio.use.sw.alac.decoder=true \
     vendor.audio.use.sw.ape.decoder=true \
-    vendor.audio.hw.aac.encoder=true \
-    vendor.audio.noisy.broadcast.delay=600 \
-    vendor.audio.offload.pstimeout.secs=3 \
-    vendor.audio.adm.buffering.ms=3 \
-    ro.af.client_heap_size_kbyte=7168 \
-    persist.vendor.audio.hw.binder.size_kbyte=1024 \
-    vendor.audio.noise.reduction.for.voice.call=false \
-    ro.config.vc_call_vol_steps=11
+    vendor.voice.path.for.pcm.voip=true
 
 # Battery
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -59,17 +59,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
-    vendor.qcom.bluetooth.soc=cherokee \
     ro.bluetooth.a4wp=false \
     ro.bluetooth.emb_wp_mode=false \
-    ro.bluetooth.wipower=false
+    ro.bluetooth.wipower=false \
+    vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.camera.HAL3.enabled=1 \
+    persist.ts.rtmakeup=false \
     persist.vendor.camera.depth.focus.cb=0 \
-    persist.vendor.camera.vdbea.switch=1 \
-    persist.ts.rtmakeup=false
+    persist.vendor.camera.HAL3.enabled=1 \
+    persist.vendor.camera.vdbea.switch=1
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -97,36 +97,36 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196610 \
+    debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
     dev.pm.dyn_samplingrate=1 \
     persist.demo.hdmirotationlock=false \
     persist.hwc.enable_vds=1 \
     persist.sys.force_sw_gles=0 \
-    vendor.gralloc.enable_fb_ubwc=1 \
-    vendor.gralloc.disable_wb_ubwc=1 \
+    ro.opengles.version=196610 \
     vendor.display.disable_partial_split=1 \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=0 \
-    vendor.display.perf_hint_window=50
+    vendor.display.perf_hint_window=50 \
+    vendor.gralloc.disable_wb_ubwc=1 \
+    vendor.gralloc.enable_fb_ubwc=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-http=true \
+    media.aac_51_output_enabled=true \
     media.stagefright.enable-aac=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-player=true \
     media.stagefright.enable-qcp=true \
     media.stagefright.enable-scan=true \
-    mmp.enable.3g2=true \
-    media.aac_51_output_enabled=true \
     mm.enable.smoothstreaming=true \
+    mmp.enable.3g2=true \
+    persist.media.treble_omx=true \
     persist.mm.enable.prefetch=true \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    persist.media.treble_omx=true
+    vidc.enc.dcvs.extra-buff-count=2
 
 # IZat
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -139,8 +139,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.nfc_nci=bcm2079x.default \
     persist.nfc.smartcard.config=SIM1,eSE1 \
+    ro.hardware.nfc_nci=bcm2079x.default \
     ro.nfc.port=I2C
 
 # OEM Unlock reporting
@@ -149,8 +149,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
     ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
     sched.colocate.enable=1 \
     sys.games.gt.prof=1
 
@@ -166,49 +166,49 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    persist.rild.nitz_plmn="" \
+    persist.radio.multisim.config=dsds \
     persist.rild.nitz_long_ons_0="" \
     persist.rild.nitz_long_ons_1="" \
     persist.rild.nitz_long_ons_2="" \
     persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_plmn="" \
     persist.rild.nitz_short_ons_0="" \
     persist.rild.nitz_short_ons_1="" \
     persist.rild.nitz_short_ons_2="" \
     persist.rild.nitz_short_ons_3="" \
-    ril.subscription.types=NV,RUIM \
-    ro.telephony.default_network=22,20 \
-    telephony.lteOnCdmaDevice=1 \
-    persist.radio.multisim.config=dsds \
+    persist.vendor.ap.restart_level=0 \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    persist.vendor.ap.restart_level=0 \
-    persist.vendor.ssr.restart_level=3
+    persist.vendor.ssr.restart_level=3 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.default_network=22,20 \
+    telephony.lteOnCdmaDevice=1 \
+    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rmnet.data.enable=true \
-    persist.data.wda.enable=true \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.ul_mode=5 \
     persist.data.df.agg.dl_pkt=10 \
     persist.data.df.agg.dl_size=4096 \
-    persist.data.df.mux_count=8 \
+    persist.data.df.dev_name=rmnet_usb0 \
+    persist.data.df.dl_mode=5 \
     persist.data.df.iwlan_mux=9 \
-    persist.data.df.dev_name=rmnet_usb0
+    persist.data.df.mux_count=8 \
+    persist.data.df.ul_mode=5 \
+    persist.data.wda.enable=true \
+    persist.rmnet.data.enable=true
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.sensors.direct_channel=true \
     ro.vendor.sensors.dev_ori=true \
-    ro.vendor.sensors.pmd=true \
-    ro.vendor.sensors.sta_detect=true \
-    ro.vendor.sensors.mot_detect=true \
     ro.vendor.sensors.dpc=true \
+    ro.vendor.sensors.mot_detect=true \
     ro.vendor.sensors.multishake=true \
-    persist.vendor.sensors.direct_channel=true
+    ro.vendor.sensors.pmd=true \
+    ro.vendor.sensors.sta_detect=true
 
 # Shutdown
 PRODUCT_PROPERTY_OVERRIDES += \
