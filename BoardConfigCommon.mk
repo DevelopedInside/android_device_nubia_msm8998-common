fb15d4cd4e28bb1bf6d700a290dde7a284b6c623
BeYkeRYkt <beykerykt@gmail.com>
2020-04-11 06:20:55 +0900

    msm8998-common: enable full dexpreopt
    Change-Id: I787726904fca49c407d6a2b897303e250af078af

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -195,10 +195,12 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(PLATFORM_PATH)/config.fs
