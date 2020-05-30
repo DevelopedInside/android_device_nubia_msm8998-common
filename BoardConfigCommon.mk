e939612355c5ab061a68f25ae4adebf1443d1290
BeYkeRYkt <beykerykt@gmail.com>
2020-05-30 16:58:43 +0900

    msm8998-common: Update vendor security patch level
    * NubiaUI 6.27
    
    Change-Id: I9c68daea6f333fc2a9e7ccc882ae56ffe84f0344

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -261,7 +261,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /mnt/vendor/persist:/persist
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-02-05
+VENDOR_SECURITY_PATCH := 2020-04-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
