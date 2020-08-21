ba664bd582305181b3643f813b2820d453079c70
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    msm8998-common: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- msm8998.mk
+++ msm8998.mk
@@ -410,6 +410,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
