49dcfaa26c3ac0669836501370f62d64a38f035a
BeYkeRYkt <beykerykt@gmail.com>
2020-12-25 00:46:32 +0900

    msm8998-common: audio: Drop audio_configs.xml
    * Not used.
    
    Change-Id: I1ef9a4fbc4d08e6ef416cb5290e7d3f3efda6cc2

--- msm8998.mk
+++ msm8998.mk
@@ -98,7 +98,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_configs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_configs.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
