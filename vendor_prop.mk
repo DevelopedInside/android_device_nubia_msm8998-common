42b502ad202ab7069e4bd8ff1e2e62b422a0ac3c
BeYkeRYkt <beykerykt@gmail.com>
2020-12-25 19:17:05 +0900

    msm8998-common: Cleanup properties
    Change-Id: I5f7a8e4037f49d497a8a79e7f4c9289ac2f60e55

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -34,8 +34,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
     ro.af.client_heap_size_kbyte=7168 \
     ro.bluetooth.a2dp_offload.supported=false \
-    ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=11 \
     ro.vendor.audio.sdk.fluencetype=fluence \
     ro.vendor.audio.sdk.ssr=false \
     vendor.audio.dolby.ds2.enabled=false \
@@ -100,68 +98,42 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.volume.headset.gain.depcal=true \
     vendor.voice.path.for.pcm.voip=true
 
-# Battery
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cutoff_voltage_mv=3200
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=11
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    bt.max.hfpclient.connections=1 \
-    ro.bluetooth.a4wp=false \
-    ro.bluetooth.emb_wp_mode=false \
-    ro.bluetooth.wipower=false \
     vendor.qcom.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.ts.rtmakeup=false \
     persist.vendor.camera.depth.focus.cb=0 \
     persist.vendor.camera.HAL3.enabled=1 \
-    persist.vendor.camera.vdbea.switch=1
+    persist.vendor.camera.vdbea.switch=1 \
+    vidc.enc.dcvs.extra-buff-count=2
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
 
-# Core control
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.core_ctl_min_cpu=2 \
-    ro.vendor.qti.core_ctl_max_cpu=4
-
-# Data
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.netmgrd.qos.enable=true \
-    persist.radio.aosp_usr_pref_sel=true \
-    persist.vendor.data.mode=concurrent \
-    ro.vendor.use_data_netmgrd=true
-
-# DRM
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true
-
-# FRP
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.frp.pst=/dev/block/bootdevice/by-name/frp
-
-# Graphics
+# Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
-    dev.pm.dyn_samplingrate=1 \
-    persist.demo.hdmirotationlock=false \
-    persist.hwc.enable_vds=1 \
-    persist.sys.force_sw_gles=0 \
     ro.opengles.version=196610 \
     vendor.display.disable_partial_split=1 \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.perf_hint_window=50 \
-    vendor.gralloc.disable_wb_ubwc=1 \
     vendor.gralloc.enable_fb_ubwc=1
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -171,38 +143,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
-    media.aac_51_output_enabled=true \
-    media.stagefright.enable-aac=true \
-    media.stagefright.enable-http=true \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-qcp=true \
-    media.stagefright.enable-scan=true \
     media.stagefright.thumbnail.prefer_hw_codecs=true \
-    mm.enable.smoothstreaming=true \
-    mmp.enable.3g2=true \
-    persist.media.treble_omx=true \
-    persist.mm.enable.prefetch=true \
-    vendor.vidc.enc.disable.pq=true \
-    vidc.enc.dcvs.extra-buff-count=2
-
-# IZat
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.overlay.izat.optin=rro
+    vendor.vidc.enc.disable.pq=true
 
 # Network manager
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.data.iwlan.enable=true
-
-# Network Location Provider
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.location.osnlp.package=com.google.android.gms \
-    ro.location.osnlp.region.package=""
-
-# NFC
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.nfc.smartcard.config=SIM1,eSE1 \
-    ro.hardware.nfc_nci=bcm2079x.default \
-    ro.nfc.port=I2C
 
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -211,55 +157,28 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
-    sched.colocate.enable=1 \
-    sys.games.gt.prof=1
-
-# QCOM
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qcomsysd.enabled=1
-
-# QTI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.at_library=libqti-at.so \
-    ro.vendor.gt_library=libqti-gt.so
+    ro.vendor.qti.core_ctl_max_cpu=4 \
+    ro.vendor.qti.core_ctl_min_cpu=2 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
     persist.radio.multisim.config=dsds \
-    persist.rild.nitz_long_ons_0="" \
-    persist.rild.nitz_long_ons_1="" \
-    persist.rild.nitz_long_ons_2="" \
-    persist.rild.nitz_long_ons_3="" \
-    persist.rild.nitz_plmn="" \
-    persist.rild.nitz_short_ons_0="" \
-    persist.rild.nitz_short_ons_1="" \
-    persist.rild.nitz_short_ons_2="" \
-    persist.rild.nitz_short_ons_3="" \
     persist.vendor.ap.restart_level=0 \
+    persist.vendor.data.mode=concurrent \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.ssr.restart_level=3 \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=22,20 \
-    telephony.lteOnCdmaDevice=1 \
+    ro.vendor.use_data_netmgrd=true \
     vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
-# RmNet Data
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.data.df.agg.dl_pkt=10 \
-    persist.data.df.agg.dl_size=4096 \
-    persist.data.df.dev_name=rmnet_usb0 \
-    persist.data.df.dl_mode=5 \
-    persist.data.df.iwlan_mux=9 \
-    persist.data.df.mux_count=8 \
-    persist.data.df.ul_mode=5 \
-    persist.data.wda.enable=true \
-    persist.rmnet.data.enable=true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    DEVICE_PROVISIONED=1 \
+    ril.subscription.types=NV,RUIM \
+    telephony.lteOnCdmaDevice=1
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -270,10 +189,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.multishake=true \
     ro.vendor.sensors.pmd=true \
     ro.vendor.sensors.sta_detect=true
-
-# Shutdown
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.vendor.shutdown.waittime=500
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
@@ -291,7 +206,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Times services
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.timed.enable=true
+    persist.vendor.delta_time.enable=true
 
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
