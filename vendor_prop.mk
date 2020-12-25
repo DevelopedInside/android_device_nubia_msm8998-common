5be8339172c2eb03c74815924c7afc7864acfaa1
BeYkeRYkt <beykerykt@gmail.com>
2020-12-25 22:59:59 +0900

    msm8998-common: Force enable VoLTE
    Change-Id: I5510defbf708dc665634f8ec8c4766b0b97dc200

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -137,6 +137,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
 # Media
