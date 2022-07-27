6722a8c6e9cb817812a1da4617e248decb119b64
Vladimir Mikhailov <beykerykt@gmail.com>
2022-07-27 17:53:04 +0900

    msm8998-common: Move powerhint to vendor
    Change-Id: I9aaca132c589598a8bc93bf05fba3877e78e146b

--- msm8998.mk
+++ msm8998.mk
@@ -290,9 +290,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@1.0.vendor
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
